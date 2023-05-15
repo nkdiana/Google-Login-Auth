643ad559cbddf1179f74c97d9262cc821b09da58
nkdiana <dekanudiana@gmail.com>
2023-05-15 15:37:59 +0100

    google login

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('auth/google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.appService.googleLogin(req);
+  }
+}
